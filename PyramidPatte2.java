63b3358f2d9e43c1a73e7e20ce9ca062716a81c8
hyper-27 <anujdangi227@gmail.com>
2025-03-02 22:17:54 +0530

    Add files via upload

---
+++ PyramidPatte2.java
@@ -0,0 +1,20 @@
+public class PyramidPatte2
+{
+public static void main(String args[])
+ {
+ int i, j, k=8;
+ for(i=0; i<5; i++)
+ {
+ for(j=0; j<k; j++)
+ {
+ System.out.print(" ");
+ }
+ k = k - 2;
+ for(j=0; j<=i; j++)
+ {
+ System.out.print("* ");
+ }
+ System.out.println();
+ }
+ }
+}
