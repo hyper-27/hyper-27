9b0e83c8ce2a25348faa06b701efb5866a90e16e
hyper-27 <anujdangi227@gmail.com>
2025-03-02 22:18:41 +0530

    Add files via upload

---
+++ character.java
@@ -0,0 +1,14 @@
+public class character
+{
+public static void main(String [] args){
+char ch = 'a';
+ch=99;
+ch=(char)(ch + 2);
+System.out.println(ch);
+int in;
+in= ch;
+System.out.println(in);
+}
+}
+
+
